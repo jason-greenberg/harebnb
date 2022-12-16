4a5b21474f81c2fc1ddea26d94e79589b51f456f
jason-greenberg <jasongreenberg07@gmail.com>
2022-12-16 13:27:13 -0800

    Remove redundantly nested root folder

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -7,6 +7,12 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 // ...
 const validateSignup = [
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a first name'),
+  check('lastName')
+    .exists({ checkFalsy: true})
+    .withMessage('Please provide a last name'),
   check('email')
     .exists({ checkFalsy: true })
     .isEmail()
@@ -32,7 +38,7 @@ router.post(
   validateSignup,
   async (req, res) => {
     const { email, password, username } = req.body;
-    const user = await User.signup({ email, username, password });
+    const user = await User.signup({ firstName, lastName, email, username, password });
 
     await setTokenCookie(res, user);
 
