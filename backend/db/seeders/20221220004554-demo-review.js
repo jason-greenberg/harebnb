0505207b9a50417513898f4310ad59592ee48d5a
jason-greenberg <jasongreenberg07@gmail.com>
2023-01-11 15:17:12 -0800

    Add additional seed data review images, reviews

--- backend/db/seeders/20221220004554-demo-review.js
+++ backend/db/seeders/20221220004554-demo-review.js
@@ -12,38 +12,112 @@ module.exports = {
       {
         userId: 1,
         spotId: 1,
-        review: 'Great spot! Loved the basket-weaving classes.',
+        review: "Yo this spot was lit, the basket-weaving classes were the move.",
+        stars: 5
+      },
+      {
+        userId: 1,
+        spotId: 2,
+        review: "Comfy spot, good location",
+        stars: 4
+      },
+      {
+        userId: 1,
+        spotId: 3,
+        review: "This crib was so cute and cozy. Definitely would hit it again",
+        stars: 5
+      },
+      {
+        userId: 1,
+        spotId: 4,
+        review: "Had a blast here, the basket-weaving classes were lit",
+        stars: 5
+      },
+      {
+        userId: 1,
+        spotId: 5,
+        review: "This spot was lit, conveniently located and cozy asf",
+        stars: 4
+      },
+      {
+        userId: 1,
+        spotId: 6,
+        review: "Had the best time ever here, the decorations were on point",
+        stars: 5
+      },
+      //
+      {
+        userId: 2,
+        spotId: 1,
+        review: "This spot was a vibe, loved the countryside feel",
         stars: 5
       },
       {
         userId: 2,
         spotId: 2,
-        review: 'Comfortable and convenient location.',
+        review: "Loved this place, felt like a part of a magical bunny kingdom",
+        stars: 4
+      },
+      {
+        userId: 2,
+        spotId: 3,
+        review: "Cute and unique asf, highly recommend",
+        stars: 5
+      },
+      {
+        userId: 2,
+        spotId: 4,
+        review: "Loved this spot, perfect for a peaceful getaway",
+        stars: 5
+      },
+      {
+        userId: 2,
+        spotId: 5,
+        review: "This spot was so fun and unique, 10/10 would recommend",
+        stars: 4
+      },
+      {
+        userId: 2,
+        spotId: 6,
+        review: "Had the best time ever, definitely coming back",
+        stars: 5
+      },
+      //
+      {
+        userId: 3,
+        spotId: 1,
+        review: "Loved everything about this spot, the decorations were on point",
+        stars: 5
+      },
+      {
+        userId: 3,
+        spotId: 2,
+        review: "Such a cozy and peaceful spot, had a great stay",
         stars: 4
       },
       {
         userId: 3,
         spotId: 3,
-        review: 'Charming and cozy home. Would stay again.',
-        stars: 4
-      },
-      {
-        userId: 2,
-        spotId: 1,
-        review: 'I had a great time here! The basket-weaving classes were so much fun.',
+        review: "This spot was so charming and had a great location",
         stars: 5
       },
       {
-        userId: 2,
-        spotId: 2,
-        review: 'This was a great place to stay. The location was convenient and the home was comfortable.',
+        userId: 3,
+        spotId: 4,
+        review: "Had a great stay here, loved the countryside vibe",
+        stars: 5
+      },
+      {
+        userId: 3,
+        spotId: 5,
+        review: "This place was so cozy and had the best decorations",
         stars: 4
       },
       {
-        userId: 2,
-        spotId: 3,
-        review: 'I loved staying at this cozy home. It was charming and I would definitely stay here again.',
-        stars: 4
+        userId: 3,
+        spotId: 6,
+        review: 'I loved my stay! I would come back again.',
+        stars: 5
       },
   ], {});
 },
