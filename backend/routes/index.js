0c82b2392637aeffe1928cf4cef30767a04c5317
jason-greenberg <jasongreenberg07@gmail.com>
2023-02-09 14:16:45 -0800

    remove csrfFetch test route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -28,11 +28,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
-
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
