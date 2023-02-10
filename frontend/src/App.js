81f91913c3dbc3b4a7c881d9e8bcd21db9ff38ba
jason-greenberg <jasongreenberg07@gmail.com>
2023-02-10 10:10:31 -0800

    Create Navigation component, enable Font Awesome for icons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Switch, Route } from 'react-router-dom'
 import LoginFormPage from './components/LoginFormPage';
+import Navigation from './components/Navigation';
 import SignupFormPage from './components/SignupFormPage';
 import { restoreUser } from './store/session';
 
@@ -16,11 +17,15 @@ function App() {
 
 
   return (
-    <Switch>
-      <Route path='/login' component={LoginFormPage} />
-      <Route path='/signup' component={SignupFormPage} />
-      <h1>Home</h1>
-    </Switch>
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && (
+        <Switch>
+          <Route path='/login' component={LoginFormPage} />
+          <Route path='/signup' component={SignupFormPage} />
+          <h1>Home</h1>
+        </Switch>)}
+    </>
   );
 }
 
