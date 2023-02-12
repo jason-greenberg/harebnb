bf9067bd21f1ebe892dbd169a417f1c209333474
jason-greenberg <jasongreenberg07@gmail.com>
2023-02-12 12:25:12 -0800

    Create AllSpots and SpotsDetailCard components

--- frontend/src/components/Spots/AllSpots.js
+++ frontend/src/components/Spots/AllSpots.js
@@ -1,6 +1,25 @@
+import { useSelector } from 'react-redux';
+import SpotDetail from './SpotDetailCard';
+import './Spots.css'
+
 function AllSpots() {
+  const allSpots = useSelector(state => state.spots.allSpots);
+  const allSpotsArray = Object.values(allSpots);
+
   return (
-    <h1>All Spots</h1>
+    <>
+      <h1>All Spots</h1>
+      <ul className="spots-list">
+        {
+          allSpotsArray.length > 0
+          && allSpotsArray.map(spot => (
+            <li>
+              <SpotDetail spot={spot} />
+            </li>
+          ))
+        }
+      </ul>
+    </>
   )
 }
 
---
+++ frontend/src/components/Spots/SpotDetailCard.js
@@ -0,0 +1,11 @@
+function SpotDetail({ spot }) {
+  return (
+    <>
+      <h2>Spot Detail</h2>
+      <p>{spot.id}</p>
+      <p>detail</p>
+    </>
+  )
+}
+
+export default SpotDetail;
