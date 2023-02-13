35d4bad36857279e1fecb9d97645b8dfc871a798
jason-greenberg <jasongreenberg07@gmail.com>
2023-02-13 15:46:04 -0800

    Add styling SpotDetails component

--- frontend/src/components/Spots/SpotDetails/SpotDetails.js
+++ frontend/src/components/Spots/SpotDetails/SpotDetails.js
@@ -8,22 +8,52 @@ function SpotDetails() {
   const { spotId } = useParams();
   const dispatch = useDispatch();
   const spot = useSelector(state => state.spots.singleSpot);
-  const preview = spot.SpotImages[0].url;
-
+  
   useEffect(() => {
     dispatch(getSingleSpotData(spotId));
   }, [spotId])
 
+  // Return Spot Not Found, if spot does not exist in spots.singleSpot slice of state
+  if (!spot || !Object.values(spot).length) {
+    return <h1 className="not-found">Spot Not Found</h1>
+  }
+
+  const preview = spot.SpotImages[0].url;
+
+
   return (
-    <div className="spot-detail-container">
-      <h2>{spot.name}</h2>
-      <h3>{`${spot.city}, ${spot.state}, ${spot.country}`}</h3>
-      <div className="spot-detail-images-container">
-        <div className="spot-detail-preview-image">
-          <img src={preview} alt="preview-image" />
+    <div className="spot-details-container">
+      <div className="name-location">
+        <h2>{spot.name}</h2>
+        <h3>{`${spot.city}, ${spot.state}, ${spot.country}`}</h3>
+      </div>
+      <div className="spot-details-images-container">
+        <div className="large-preview-image-contanier">
+          <img 
+            src={preview && preview} 
+            alt="preview-image"
+            className="large-preview-image"
+          />
+        </div>
+        <div className="small-preview-images-container">
+          {/* {Maps the second through fifth SpotImages to the page} */}
+          {spot.SpotImages && spot.SpotImages.slice(1, 5).map(image => (
+            <img 
+              key={image.id} 
+              className="small-preview-image"
+              src={image.url}
+              alt="preview-image"
+            >
+            </img>
+          ))}
         </div>
       </div>
-      <p>{spot.address}</p>
+      <div className="description-container">
+        <div className="host-name-description"></div>
+        <div className="price-reviews-reserve"></div>
+      </div>
+      <hr className="hr"/>
+
     </div>
   )
 }
