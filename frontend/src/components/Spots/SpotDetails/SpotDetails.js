671fe133ef4c6db208fa2a1b682822a3f65515d7
jason-greenberg <jasongreenberg07@gmail.com>
2023-02-13 10:34:25 -0800

    Initial build out of SpotDetails component

--- frontend/src/components/Spots/SpotDetails/SpotDetails.js
+++ frontend/src/components/Spots/SpotDetails/SpotDetails.js
@@ -2,11 +2,13 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom"
 import { getSingleSpotData } from "../../../store/spots";
+import './SpotDetails.css'
 
 function SpotDetails() {
   const { spotId } = useParams();
   const dispatch = useDispatch();
   const spot = useSelector(state => state.spots.singleSpot);
+  const preview = spot.SpotImages[0].url;
 
   useEffect(() => {
     dispatch(getSingleSpotData(spotId));
@@ -14,8 +16,13 @@ function SpotDetails() {
 
   return (
     <div className="spot-detail-container">
-      <h1>Spot Details</h1>
-      <p>{spot.id}</p>
+      <h2>{spot.name}</h2>
+      <h3>{`${spot.city}, ${spot.state}, ${spot.country}`}</h3>
+      <div className="spot-detail-images-container">
+        <div className="spot-detail-preview-image">
+          <img src={preview} alt="preview-image" />
+        </div>
+      </div>
       <p>{spot.address}</p>
     </div>
   )
