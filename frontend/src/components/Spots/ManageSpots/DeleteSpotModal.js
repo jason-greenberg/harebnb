4ec9aaa4958a9a8b1d664cc733ca15b71e3318fd
jason-greenberg <jasongreenberg07@gmail.com>
2023-02-15 12:46:54 -0800

    Build out delete modal

--- frontend/src/components/Spots/ManageSpots/DeleteSpotModal.js
+++ frontend/src/components/Spots/ManageSpots/DeleteSpotModal.js
@@ -2,7 +2,14 @@ import { useModal } from "../../../context/Modal";
 
 function DeleteSpotModal() {
   return (
-    <p>Are you sure you want to remove this spot from the listings</p>
+    <div className="delete-modal-container">
+      <h1>Confirm Delete</h1>
+      <p>Are you sure you want to remove this spot from the listings</p>
+      <div className="confirm-delete-buttons">
+        <button className="yes-delete-button button">{`Yes (Delete Spot)`}</button>
+        <button className="no-keep-spot-button button">{`No (Keep Spot)`}</button>
+      </div>
+    </div>
   )
 }
 
