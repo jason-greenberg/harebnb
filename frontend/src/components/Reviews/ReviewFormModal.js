d91976d3e627e223005befedb1c0b9930b2fb3ec
jason-greenberg <jasongreenberg07@gmail.com>
2023-02-15 17:09:39 -0800

    Build out ReviewFormModal component

--- frontend/src/components/Reviews/ReviewFormModal.js
+++ frontend/src/components/Reviews/ReviewFormModal.js
@@ -2,7 +2,22 @@ import './ReviewFormModal.css';
 
 function ReviewFormModal() {
   return (
-    <h1>How was your stay?</h1>
+    <div className="review-form-modal-container">
+      <h1>How was your stay?</h1>
+      <textarea 
+        cols="30" 
+        rows="10"
+        placeholder='Leave your review here...'
+      >
+      </textarea>
+      <div className="starts-container">
+        <div className="star-symbols"></div>
+        <div>Stars</div>
+      </div>
+      <div className="submit">
+        <button>Submit Your Review</button>
+      </div>
+    </div>
   )
 }
 
